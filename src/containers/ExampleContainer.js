2f20176994f14cc733c42bcde90d576d4cab88a3
FredHu <hufei_yx@sina.com>
2018-12-10 23:49:14 +0800

    update

--- src/containers/ExampleContainer.js
+++ src/containers/ExampleContainer.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -7,49 +6,101 @@ import request from 'tools/request';
 import { Button } from 'antd';
 import axios from 'axios';
 import 'static/logo.png';
+import { Table, Divider, Tag } from 'antd';
 class Test extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.focus = this.focus.bind(this);
   }
-  componentDidMount() {
-    
-  }
+  componentDidMount() {}
   focus() {
     // alert();
   }
-  render(){
-    return <input placeholder='hello'/>
+  render() {
+    return <input placeholder="hello" />;
   }
 }
 class CompExampleContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      columns: [
+        {
+          title: '名字',
+          dataIndex: 'name',
+          key: 'name',
+        },
+        {
+          title: '地址',
+          dataIndex: 'url',
+          key: 'url',
+        }
+      ],
+      datas: []
     };
   }
   componentWillMount() {
-    this.test()
+    this.test();
   }
   componentDidMount() {
-    this.inp.focus()
+    this.inp.focus();
   }
-  test = ()=>{
-    console.log(1111)
+  test = () => {
+    console.log(1111);
   };
+  ajaxList() {
+    let _this = this;
+    request
+      .get('/curd/list')
+      .then(function(response) {
+        response.data.forEach(v=>{
+          v.key= v['_id']
+        })
+        _this.setState({
+          datas:response.data
+        })
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
+  ajaxAdd() {
+    let _this = this;
+    request
+      .post('/curd/add')
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
   render() {
-    const {count,data,IncreaseCount,loadData,count1} = this.props;
+    const { count, data, IncreaseCount, loadData, count1 } = this.props;
+    let { columns, datas } = this.state;
     return (
       <div>
-        <Test ref={(inp)=>{
-          this.inp = inp;
-        }}></Test>
+        <Test
+          ref={inp => {
+            this.inp = inp;
+          }}
+        />
         <Button type="primary">Primary</Button>
         <button onClick={IncreaseCount}>点我</button>
-        <h1>{count}----{count1}</h1>
+        <h1>
+          {count}
+          ----
+          {count1}
+        </h1>
         <button onClick={loadData}>加载数据</button>
         <h1>{JSON.stringify(data)}</h1>
+        <Button type="primary" onClick={this.ajaxList.bind(this)}>
+          请求数据
+        </Button>
+        <Button type="primary" onClick={this.ajaxAdd.bind(this)}>
+          增加数据
+        </Button>
+        <Table columns={columns} dataSource={datas} />
       </div>
     );
   }
@@ -59,31 +110,37 @@ CompExampleContainer.defaultProps = {};
 CompExampleContainer.propTypes = {};
 function mapStateToProps(state) {
   return {
-    count:state.exampleReducer.count,
-    count1:state.ajaxReducer.count,
-    data:state.ajaxReducer.data
+    count: state.exampleReducer.count,
+    count1: state.ajaxReducer.count,
+    data: state.ajaxReducer.data
   };
 }
 function mapDispatchToProps(dispatch) {
   return {
-    IncreaseCount:()=>{
+    IncreaseCount: () => {
       dispatch({
         type: 'TEST',
-        namespace:'ajaxReducer'
-      })
+        namespace: 'ajaxReducer'
+      });
     },
-    loadData:()=>{
-      dispatch(()=>{
-        return axios.get(config.staticlist).then(function (response) {
-          dispatch({
-            type:"AJAX",
-            data:response.data
+    loadData: () => {
+      dispatch(() => {
+        return axios
+          .get(config.staticlist)
+          .then(function(response) {
+            dispatch({
+              type: 'AJAX',
+              data: response.data
+            });
+          })
+          .catch(function(err) {
+            console.log(err);
           });
-        }).catch(function (err) {
-          console.log(err);
-        });
       });
     }
   };
 }
-export default connect(mapStateToProps, mapDispatchToProps)(CompExampleContainer);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CompExampleContainer);
