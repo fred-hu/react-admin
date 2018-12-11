305def27ab6b0f8329b1dc5931e6e25a5a552b34
FredHu <hufei_yx@sina.com>
2018-12-11 23:22:09 +0800

    设置主页

--- src/containers/ExampleContainer.js
+++ src/containers/ExampleContainer.js
@@ -6,34 +6,71 @@ import request from 'tools/request';
 import { Button } from 'antd';
 import axios from 'axios';
 import 'static/logo.png';
-import { Table, Divider, Tag } from 'antd';
-class Test extends React.Component {
-  constructor(props) {
-    super(props);
-    this.focus = this.focus.bind(this);
-  }
-  componentDidMount() {}
-  focus() {
-    // alert();
-  }
-  render() {
-    return <input placeholder="hello" />;
-  }
-}
+import {
+  Table,
+  Divider,
+  Form,
+  Modal,
+  Icon,
+  Input,
+  Checkbox,
+  message
+} from 'antd';
+const FormItem = Form.Item;
+import 'styles/ExampleContainer.less';
+// class Test extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.focus = this.focus.bind(this);
+//   }
+//   componentDidMount() {}
+//   focus() {
+//     // alert();
+//   }
+//   render() {
+//     return <input placeholder="hello" />;
+//   }
+// }
 class CompExampleContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      visible: false,
       columns: [
+        {
+          title: 'id',
+          dataIndex: '_id',
+          key: '_id',
+          className: 'hide'
+        },
         {
           title: '名字',
           dataIndex: 'name',
           key: 'name',
+          width: 400
         },
         {
           title: '地址',
           dataIndex: 'url',
           key: 'url',
+          width: 400
+        },
+        {
+          title: '操作',
+          key: 'operation',
+          width: 200,
+          render: (text, record) => (
+            <span>
+              <a href="javascript:;">编辑</a>
+              <Divider type="vertical" />
+              <a
+                href="javascript:;"
+                onClick={this.delete.bind(this, record['_id'])}
+              >
+                删除
+              </a>
+            </span>
+          )
         }
       ],
       datas: []
@@ -41,9 +78,10 @@ class CompExampleContainer extends React.Component {
   }
   componentWillMount() {
     this.test();
+    this.ajaxList();
   }
   componentDidMount() {
-    this.inp.focus();
+    // this.inp.focus();
   }
   test = () => {
     console.log(1111);
@@ -53,23 +91,68 @@ class CompExampleContainer extends React.Component {
     request
       .get('/curd/list')
       .then(function(response) {
-        response.data.forEach(v=>{
-          v.key= v['_id']
-        })
+        response.data.data.forEach(v => {
+          v.key = v['_id'];
+        });
         _this.setState({
-          datas:response.data
-        })
+          datas: response.data.data
+        });
       })
       .catch(function(err) {
         console.log(err);
       });
   }
+  handleOk() {
+    let _this = this;
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log(values);
+        request
+          .post('/curd/add', values)
+          .then(function(response) {
+            _this.ajaxList();
+            console.log(response);
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
+        this.setState(
+          {
+            visible: false
+          },
+          () => {
+            this.props.form.resetFields();
+          }
+        );
+      }
+    });
+  }
+  handleCancel() {
+    this.setState(
+      {
+        visible: false
+      },
+      () => {
+        this.props.form.resetFields();
+      }
+    );
+  }
   ajaxAdd() {
+    this.setState({
+      visible: true
+    });
+  }
+  delete(id) {
     let _this = this;
     request
-      .post('/curd/add')
+      .post('/curd/delete', {
+        id
+      })
       .then(function(response) {
-        console.log(response);
+        if (response.data.success) {
+          message.success(response.data.description);
+          _this.ajaxList();
+        }
       })
       .catch(function(err) {
         console.log(err);
@@ -78,9 +161,20 @@ class CompExampleContainer extends React.Component {
   render() {
     const { count, data, IncreaseCount, loadData, count1 } = this.props;
     let { columns, datas } = this.state;
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 4 }
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 20 }
+      }
+    };
     return (
-      <div>
-        <Test
+      <div className="ExampleContainer">
+        {/* <Test
           ref={inp => {
             this.inp = inp;
           }}
@@ -93,14 +187,36 @@ class CompExampleContainer extends React.Component {
           {count1}
         </h1>
         <button onClick={loadData}>加载数据</button>
-        <h1>{JSON.stringify(data)}</h1>
-        <Button type="primary" onClick={this.ajaxList.bind(this)}>
-          请求数据
-        </Button>
-        <Button type="primary" onClick={this.ajaxAdd.bind(this)}>
-          增加数据
-        </Button>
-        <Table columns={columns} dataSource={datas} />
+        <h1>{JSON.stringify(data)}</h1> */}
+        <div style={{ marginTop: '20px' }}>
+          <div className="btns">
+            <Button type="primary" onClick={this.ajaxAdd.bind(this)}>
+              增加数据
+            </Button>
+          </div>
+          <div className="lists">
+            <Table columns={columns} dataSource={datas} />
+          </div>
+        </div>
+        <Modal
+          title="新增数据"
+          visible={this.state.visible}
+          onOk={this.handleOk.bind(this)}
+          onCancel={this.handleCancel.bind(this)}
+        >
+          <Form className="first-form">
+            <FormItem {...formItemLayout} label="name">
+              {getFieldDecorator('name', {
+                rules: [{ required: true, message: '请输入name!' }]
+              })(<Input placeholder="请输入name" />)}
+            </FormItem>
+            <FormItem {...formItemLayout} label="url">
+              {getFieldDecorator('url', {
+                rules: [{ required: true, message: '请输入url!' }]
+              })(<Input placeholder="请输入url" />)}
+            </FormItem>
+          </Form>
+        </Modal>
       </div>
     );
   }
@@ -140,7 +256,8 @@ function mapDispatchToProps(dispatch) {
     }
   };
 }
+const WrappedNormalForm = Form.create()(CompExampleContainer);
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(CompExampleContainer);
+)(WrappedNormalForm);
--- src/router.js
+++ src/router.js
@@ -1,59 +1,81 @@
-import {hot} from 'react-hot-loader';
-import React from "react";
+import { hot } from 'react-hot-loader';
+import React from 'react';
 import { connect } from 'react-redux';
-import {HashRouter as Router, Route, Link,withRouter,Switch} from "react-router-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Link,
+  withRouter,
+  Switch,
+  Redirect
+} from 'react-router-dom';
 import Navigation from './containers/Navigation';
 import TopBar from './containers/TopBar';
 import { Breadcrumb } from 'antd';
 import AppBreadcrumb from 'containers/AppBreadcrumb';
 import Home from 'containers/Home';
 
-export class AppRouter extends React.Component{
-  constructor(props){
+export class AppRouter extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={
-    
-    }
+    this.state = {};
   }
-  componentWillMount(){
-  
-  }
-  trans = (data=[],obj={})=>{
-    data.forEach((v,i)=>{
+  componentWillMount() {}
+  trans = (data = [], obj = {}) => {
+    data.forEach((v, i) => {
       obj[v.link] = v.linkName;
-      v.children && v.children.length && this.trans(v.children,obj);
+      v.children && v.children.length && this.trans(v.children, obj);
     });
     return obj;
   };
-  render(){
+  render() {
     let breadcrumbNameMap = this.trans(this.props.menu) || {};
-    
+
     return (
       <Router>
-        <div id='app-container'>
-          <div id='app-header'>
-            <TopBar/>
+        <div id="app-container">
+          <div id="app-header">
+            <TopBar />
           </div>
-          <div id='app-main'>
-            <div id='app-main-slider' style={{display:'inline-block'}}>
-              <Navigation/>
+          <div id="app-main">
+            <div id="app-main-slider" style={{ display: 'inline-block' }}>
+              <Navigation />
             </div>
-            <div id='app-main-content' style={{display:'inline-block',verticalAlign:'top',padding:20}}>
-              <AppBreadcrumb breadcrumbNameMap={breadcrumbNameMap}/>
-              <Route exact path="/" component={Home}/>
+            <div
+              id="app-main-content"
+              style={{
+                display: 'inline-block',
+                verticalAlign: 'top',
+                padding: 20
+              }}
+            >
+              <AppBreadcrumb breadcrumbNameMap={breadcrumbNameMap} />
+              <Switch>
+                <Route
+                  exact
+                  path="/"
+                  render={props => (
+                    <Redirect
+                      to={{
+                        pathname: '/one',
+                        state: { from: props.location }
+                      }}
+                    />
+                  )}
+                />
+                <Route path="/one" exact component={Home} />
+              </Switch>
             </div>
           </div>
-          <div id='app-footer'>
-      
-          </div>
+          <div id="app-footer" />
         </div>
       </Router>
-    )
+    );
   }
 }
 function mapStateToProps(state) {
   return {
-    menu:state.menuReducer.menu
+    menu: state.menuReducer.menu
   };
 }
 function mapDispatchToProps(dispatch) {
@@ -63,4 +85,9 @@ function mapDispatchToProps(dispatch) {
     //}
   };
 }
-export default hot(module)(connect(mapStateToProps, mapDispatchToProps)(AppRouter));
+export default hot(module)(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(AppRouter)
+);
