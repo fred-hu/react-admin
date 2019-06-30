24dca049cb3a90f6c5c30aeb93ad39eb21baf3eb
hufei1993 <hufei_yx@sina.com>
2019-06-30 13:33:16 +0800

    update

--- src/reducers/test.js
+++ src/reducers/test.js
@@ -1,14 +1,14 @@
-let defaultState = { namespace: 'test'};
-let test = (state = defaultState, action) => {
-  const {  } = state;
+const defaultState = { namespace: 'test' };
+const test = (state = defaultState, action) => {
+  const { } = state;
   if (action.namespace && action.namespace !== state.namespace) {
     return state;
   }
   switch (action.type) {
-    case '':
-      return Object.assign({}, state, {});
-    default:
-      return state;
+  case '':
+    return Object.assign({}, state, {});
+  default:
+    return state;
   }
 };
 export default test;
